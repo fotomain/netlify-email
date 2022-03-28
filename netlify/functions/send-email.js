205329799a92934a983d39d2289be594c1e995f7
foto888999 <84739938+foto888999@users.noreply.github.com>
2022-03-28 12:12:56 +0300

    d

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -5,40 +5,42 @@ exports.handler = async function (event, context, callback) {
   const body = JSON.parse(event.body)
 
   // Build an HTML string to represent the body of the email to be sent.
-  const html = `<div style="margin: 20px auto;">${body.body}</div>`
+  const html = `<div style="margin: 20px auto;">${(body.body)?body.body:'inline body'}</div>`
 
   // Generate test SMTP service account from ethereal.email. Only needed if you
   // don't have a real mail account for testing
   let testAccount = await nodemailer.createTestAccount()
 
   // create reusable transporter object using the default SMTP transport
-  // let transporter = nodemailer.createTransport({
-  //    service: "gmail",
-  //    auth: {
-  //
-  //     user: 'foto888999@gmail.com',
-  //     // make special app pass
-  //     pass: '----------',
-  //  },
 
-   // })
+  let transporter = nodemailer.createTransport({
+     service: "gmail",
+     auth: {
 
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-      user: 'yadira.connelly36@ethereal.email',
-      pass: 't3b5gfKWQCT2wCtahD'
-    }
-  });
+      user: 'foto888999@gmail.com',
+      // make special app password api
+      pass: 'rpezevwacplvxshd',
+   },
+
+   })
+
+  // const transporter = nodemailer.createTransport({
+  //   host: 'smtp.ethereal.email',
+  //   port: 587,
+  //   auth: {
+  //     user: 'yadira.connelly36@ethereal.email',
+  //     pass: 't3b5gfKWQCT2wCtahD'
+  //   }
+  // });
 
   try {
     // send mail with defined transport object
     let info = await transporter.sendMail({
       from: '"☁️ The Cloud ☁️" <thecloud@example.com>',
-      to: body.email,
+
+      to: (body.email)?body.email:'foto888999@gmail.com',
       subject: "New Form Submission",
-      text: body.body,
+      text: (body.body)?body.body:'inline email',
       html: html
     })
     // Log the result
