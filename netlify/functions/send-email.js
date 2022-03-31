fe863efb736d7d74f221cbfc8650df2f5945138d
foto888999 <84739938+foto888999@users.noreply.github.com>
2022-03-31 10:32:29 +0300

    w

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -34,7 +34,7 @@ exports.handler = async function (event, context, callback) {
 
   // Generate test SMTP service account from ethereal.email. Only needed if you
   // don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount()
+  // let testAccount = await nodemailer.createTestAccount()
 
   // create reusable transporter object using the default SMTP transport
 
